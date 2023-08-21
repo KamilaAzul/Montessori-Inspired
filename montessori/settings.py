f8eaa2d0d78ed0dff7604a03ccabfb979bb7c8b6
KamilaAzul <>
2023-08-21 10:23:09 +0000

    Add changes to settings file

--- montessori/settings.py
+++ montessori/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['montessori-inspired.herokuapp.com', 'localhost',
                  '8000-kamilaazul-montessori-in-ti9w1g5d77.us2.codeanyapp.com']
