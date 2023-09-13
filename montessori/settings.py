30d136afa3c6e5fa58bd67eb08773768d000bb96
Kamila Matejczuk <kamila.mate82@gmail.com>
2023-09-13 21:09:23 +0000

    debug false

--- montessori/settings.py
+++ montessori/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['montessori-inspired-a464ed5581f4.herokuapp.com', 'localhost',
                  '8000-kamilaazul-montessoriin-kv4l8sbm64d.ws-eu104.gitpod.io']
